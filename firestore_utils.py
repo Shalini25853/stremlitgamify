ca2be112d62c9baec9233122b7bbad2476d527b1
Shalini James Paulraj <80110174+Shalini25853@users.noreply.github.com>
2025-05-23 15:52:12 -0400

    Update firestore_utils.py

--- firestore_utils.py
+++ firestore_utils.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from firebase_admin import credentials, firestore
 import firebase_admin
+from firebase_admin import credentials, firestore
 
 def connect_to_firestore():
     if not firebase_admin._apps:
-        firebase_secret = dict(st.secrets["firebase"])
+        firebase_secret = dict(st.secrets["firebase"])  # this converts TOML section to a dict
         cred = credentials.Certificate(firebase_secret)
         firebase_admin.initialize_app(cred)
     return firestore.client()
